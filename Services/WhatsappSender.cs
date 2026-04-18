6217d0faef5af28e19e7b415a2709b49bda4c6c9
eulucasbatista <lucas.batista20@gmail.com>
2026-04-18 00:08:37 -0300

    feat: melhorias

---
+++ Services/WhatsappSender.cs
@@ -0,0 +1,33 @@
+﻿using NotificationSystem.Interfaces;
+using NotificationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationSystem.Services
+{
+    public class WhatsappSander : INotificationSender
+    {
+        private readonly INotificationFormatter _formatter;
+        private readonly IAppLogger _logger;
+
+        public string Channel => "Email";
+
+        public WhatsappSander(INotificationFormatter formatter, IAppLogger logger)
+        {
+            _formatter = formatter;
+            _logger = logger;
+        }
+
+        public Task SendAsync(Notification notification)
+        {
+            var body = _formatter.Format(notification);
+            _logger.LogInfo($"[WHATSAPP] Enviando para {notification.Recipient}");
+            _logger.LogInfo(body);
+            // Aqui entraria a integração real (SMTP, SendGrid, etc.)
+            return Task.CompletedTask;
+        }
+    }
+}
